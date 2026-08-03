2e6897a6feedd6778b8ac87b60095a37f36fb0ae
13Victor08 <jv.correia236@gmail.com>
2026-08-03 18:16:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-// =========================
+function calcularGestacao() {
+
+    const dum = document.getElementById("dum").value;
+
+    if (!dum) {
+        alert("Selecione a data da última menstruação.");
+        return;
+    }
+
+    const inicio = new Date(dum);
+    const hoje = new Date();
+
+    const diferenca = hoje - inicio;
+    const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+    const semanas = Math.floor(dias / 7);
+
+    document.getElementById("resultado").style.display = "block";
+    document.getElementById("semanas").innerHTML =
+        "🤰 Você está com <strong>" + semanas + " semanas</strong> de gestação.";
+
+}// =========================
 // Pais de Primeira - v3.0
 // =========================
 
