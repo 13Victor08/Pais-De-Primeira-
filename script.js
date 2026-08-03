d0f2a85382dc2bced0feec5ba6517f20d06469db
13Victor08 <jv.correia236@gmail.com>
2026-08-03 18:18:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -18,24 +18,3 @@ function calcularGestacao() {
     document.getElementById("semanas").innerHTML =
         "🤰 Você está com <strong>" + semanas + " semanas</strong> de gestação.";
 
-}// =========================
-// Pais de Primeira - v3.0
-// =========================
-
-document.addEventListener("DOMContentLoaded", () => {
-
-    console.log("Pais de Primeira carregado com sucesso!");
-
-    const botoes = document.querySelectorAll(".botao");
-
-    botoes.forEach((botao) => {
-        botao.addEventListener("mouseenter", () => {
-            botao.style.transform = "scale(1.05)";
-        });
-
-        botao.addEventListener("mouseleave", () => {
-            botao.style.transform = "scale(1)";
-        });
-    });
-
-});
